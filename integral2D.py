5835659174bd952efac37b1b977306f1111b8ac2
ysimonov <61629000+ysimonov@users.noreply.github.com>
2020-03-23 15:18:19 +0800

    Update integral2D.py

--- integral2D.py
+++ integral2D.py
@@ -2,10 +2,7 @@
 from scipy.interpolate import RectBivariateSpline
 
 def integrate_on_sphere(phi, theta, sintheta, far_field_pattern, sky_temp=None):
-
-    dtheta = theta[1] - theta[0]
-    dphi = phi[1] - phi[0]
-
+    
     if sky_temp is None: 
         integrand = far_field_pattern * sintheta
     else:
@@ -15,6 +12,8 @@ def integrate_on_sphere(phi, theta, sintheta, far_field_pattern, sky_temp=None):
     integral = RectBivariateSpline(phi, theta, integrand, \
     kx=5,ky=5).integral(phi[0],phi[-1],theta[0],theta[-1])
 
+#    dtheta = theta[1] - theta[0]
+#    dphi = phi[1] - phi[0]
 #    integral = simps(simps(integrand, theta, dtheta, 1), phi, dphi, 0)
 
     return integral
